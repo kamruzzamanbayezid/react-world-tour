353f379682924fa00792eb4c15166c5757657ad9
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-09-10 21:49:14 +0600

    added react world tour

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -3,8 +3,15 @@ import { useState } from "react";
 import Country from "../country/Country";
 
 
+
 const Countries = () => {
+
       const [countries, setCountries] = useState([]);
+
+      const [visitedCountry, setVisitedCountry] = useState([]);
+
+      const [visitedFlag, setVisitedFlag] = useState([]);
+
       useEffect((() => {
             // fetch('https://restcountries.com/v3.1/all')
             // .then(response=>response.json())
@@ -20,12 +27,49 @@ const Countries = () => {
             }
             fetchCountryData();
       }), [])
+
+      const handleToAddVisitedCountry = (country) => {
+            const newVisitedCountry = [...visitedCountry, country];
+            setVisitedCountry(newVisitedCountry);
+      }
+
+      const handleVisitedFlag = (flag) => {
+            const newVisitedFlag = [...visitedFlag, flag];
+            setVisitedFlag(newVisitedFlag)
+      }
+      console.log(visitedFlag);
       return (
             <div>
                   <h3 className="text-3xl font-medium">Countries: {countries.length}</h3>
+
+                  <div className="mt-4">
+                        <h3 className="text-3xl">Visited Countries: {visitedCountry.length}</h3>
+                        {/* <img src={flags.png} alt="" /> */}
+                        <ul className="ml-10">
+                              {
+                                    visitedCountry.map(country => <li key={country.name.common} className="font-bold list-decimal">{country.name.common}</li>)
+                              }
+                        </ul>
+                  </div>
+
+                  <div className="mt-4">
+                        <h3 className="text-3xl">Visited Flag: {visitedFlag.length}</h3>
+                        <ul className="ml-10 grid grid-cols-10 gap-2">
+                              {
+                                    visitedFlag.map((flag, index) => <img className="w-[100px]  h-[50px]" key={index} src={flag.flags.png} alt="" />)
+                              }
+                        </ul>
+                  </div>
+
                   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                         {
-                              countries.map(country => <Country country={country} key={country.cca3}></Country>)
+                              countries.map(country =>
+                                    <Country
+                                          country={country}
+                                          key={country.cca3}
+                                          handleToAddVisitedCountry={handleToAddVisitedCountry}
+                                          handleVisitedFlag={handleVisitedFlag}>
+                                    </Country>)
                         }
                   </div>
             </div>
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,30 +1,40 @@
 
 import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
-      console.log(country);
+const Country = ({ country, handleToAddVisitedCountry, handleVisitedFlag }) => {
+      // console.log(country);
       const { name, flags, population, area, independent } = country;
 
-      const [visited,setVisited] = useState(false);
+      const [visited, setVisited] = useState(false);
 
       const handleVisit = () => {
             setVisited(!visited);
+            handleVisitedFlag(country);
       }
+
       // const handleVisit = () => {
       //       setVisited(true);
       // }
 
+      // console.log(handleToAddVisitedCountry);
+
       return (
-            <div className='country'>
-                  <h3 className='text-2xl mb-3 font-medium'>Country Name: {name.common}</h3>
+            <div className={`country ${visited && 'visitedCountry'}`}>
+                  <h3 style={{ color: visited ? 'red' : 'black' }} className='text-2xl mb-3 font-medium'>Country Name: {name.common}</h3>
                   <img className='w-[400px] h-[150px]' src={flags.png} alt="" />
                   <h3 className='text-xl font-normal'>Population: {population}</h3>
                   <h3 className='font-bold'>Area: {area}</h3>
                   <h3 className='font-bold'>Is Independent: {independent ? 'True' : 'False'}</h3>
-                  <h3 className=' mb-4'>{visited?"I have visited it":"I haven't visit yet"}</h3>
-                  <button onClick={handleVisit} className='btn btn-neutral'>{visited ? 'Visited' : 'Visit'}</button>
-                  
-            </div>
+                  <h3 style={{ color: visited && 'red', fontWeight: visited && 'bold' }} className=' mb-4'>{visited ? "I have visited it" : "I haven't visit yet"}</h3>
+                  <div className='flex justify-between'>
+                        <button onClick={handleVisit} className='btn btn-neutral'>{visited ? 'Visited' : 'Visit'}</button>
+
+                        <button onClick={() => handleToAddVisitedCountry(country)} className='btn btn-neutral'>Add visit lish</button>
+                  </div>
+
+                  <button className='mt-4 w-full btn btn-outline'>Country I'll visit</button>
+
+            </div >
       );
 };
 
